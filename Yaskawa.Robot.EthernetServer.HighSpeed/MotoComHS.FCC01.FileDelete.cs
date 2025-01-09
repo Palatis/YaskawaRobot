5d9158a21dddd3ac423ae456402cc23baa421575
Victor Tseng <palatis@gmail.com>
2025-01-10 03:40:46 +0800

    code clean-up

--- Yaskawa.Robot.EthernetServer.HighSpeed/MotoComHS.FCC01.FileDelete.cs
+++ Yaskawa.Robot.EthernetServer.HighSpeed/MotoComHS.FCC01.FileDelete.cs
@@ -2,17 +2,5 @@
 {
     partial class MotoComHS
     {
-        //Untest
-        /*
-        public int FileDelete(string fileName, out ushort err_code)
-        {
-            byte[] bytes = utf_8.GetBytes(fileName);
-            var req = new PacketReq(PacketHeader.HEADER_DIVISION_FILE_CONTROL, NextRequestId(),
-                0x0, 0x0, 0x0, 0x09,
-                bytes, (ushort)bytes.Length);
-            var ans = Transmit(req.ToBytes(), PORT_FILE_CONTROL);
-            err_code = ans.added_status;
-            return ans.status;
-        }*/
     }
 }
